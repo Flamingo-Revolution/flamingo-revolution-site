68f525772dc900de83590a25d8b2734912d97b17
John Doe <email@example.com>
2026-07-22 00:14:54 +0200

    Update Mock API for testing

--- src/pages/api/test/index.ts
+++ src/pages/api/test/index.ts
@@ -1,5 +1,8 @@
-export function GET({ params, request }) {
-	
+export const prerender = false;
+
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = () => {
 	return new Response(
 		JSON.stringify({
 			name: "Astro",
@@ -7,4 +10,3 @@ export function GET({ params, request }) {
 		}),
 	);
 };
-
